ba0b60bf92274c6401a72988f85335ab1351eb94
Joshua Hecker <jhecker2001@gmail.com>
2023-11-29 10:14:25 -0600

    Added functionality to clean up a messy title as well as write the list of movies to a text file.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import html
 
 response = requests.get("https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/")
 response.raise_for_status()
@@ -7,7 +8,19 @@ webpage = response.text
 
 soup = BeautifulSoup(webpage, "html.parser")
 
-movie_titles = list(reversed([movie.getText() for movie in soup.find_all(name="h3", class_="title")]))
-print(movie_titles)
+movie_titles = list(reversed([html.unescape(movie.getText()) for movie in soup.find_all(name="h3", class_="title")]))
+# print(movie_titles)
 
+cleanup = movie_titles[58].split()
+cleanup.pop(2)
+movie_titles[58] = " ".join(cleanup)
+# print(cleanup)
+#
+# print(movie_titles[58])
 
+for m in movie_titles:
+    print(m)
+
+with open(file="./movies.txt", mode="w") as file:
+    for movie in movie_titles:
+        file.write(f"{movie}\n")
